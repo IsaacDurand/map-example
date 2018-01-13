7e56c8e6017e244523db3b165eadbc63093e34be
Isaac Durand <IsaacDurand@users.noreply.github.com>
2018-01-13 10:36:39 -0800

    Add basic form satisfying spec requirements

---
+++ README.md
@@ -0,0 +1,8 @@
+The code in this repo is designed to accompany [The mystery of the missing map: Understanding ng-show and ng-if](https://medium.com/p/f3c78d04d338).
+
+To run this code on your machine:
+1. Clone the repo
+1. Check out the branch you're interested in (there's one branch for each chapter of the story)
+1. From the command line, run `python -m SimpleHTTPServer`
+  * If you simply open `index.html` in your browser, the AngularJS component templates may not load.
+1. In your browser, visit [localhost:8000](http://localhost:8000/) (or whichever port SimpleHTTPServer is using)
---
+++ main.js
@@ -0,0 +1,30 @@
+/* Following the quickstart instructions for angular-google-maps, I'm declaring
+uiGmapgoogle-maps as a dependency of my app. */
+angular.module('app', ['uiGmapgoogle-maps'])
+/* Normally, I'd declare each component in a separate file, but I'm using one
+file for this example to keep things simple.
+As I built out these components, I'd also add bindings and controllers as needed.
+*/
+  .component('orderForm', {
+    templateUrl: 'order_form.html',
+  })
+  .component('mapTab', {
+    bindings: {
+      visible: '<',
+    },
+    templateUrl: 'map_tab.html',
+    controller: function () {
+      /* Following the quickstart instructions for angular-google-maps, I'm
+      creating a settings object to pass to the ui-gmap-google-map directive. */
+      this.map = {
+        center: {
+          latitude: 34,
+          longitude: -118,
+        },
+        zoom: 9,
+      };
+    },
+  })
+  .component('otherTab', {
+    templateUrl: 'other_tab.html',
+  });
